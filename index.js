3c4ae9601ebc96d92eff82e8dc6d398e70c97c8f
BERNARDO LANG MACHADO <bernardo_l_machado@estudante.sesisenai.org.br>
2024-07-04 17:44:26 -0300

    alteracaodb

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express')
-const { Client } = require('pg')
-require('dotenv').config()
-
-const client = new Client({
-    host: process.env.host,
-    port: process.env.port,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
-})
+require('dotenv').config()  
+const connectDB = require('./db')
 
 const app = express()
+connectDB()
  
- const connectDB = async () => {
-    client.connect()
-    .then(() => {
-        console.log('a conexao funciona')
-    }).catch((err) => {
-        console.error('erro ao conectar na db')
-    });
- };
- connectDB()
 
- app.get('/test-api', function (req, res){
-     res.send('NOSSA API TA FUNCIONANDO')
- })
+app.get('/test-api', function (req, res){
+    res.send('NOSSA API TA FUNCIONANDO')
+})
 app.listen(8000)
+ 
